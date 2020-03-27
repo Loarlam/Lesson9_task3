ccddeef7780890263174c0f0d9fdb274518683f5
Loarlam <messiah02@mail.ru>
2020-03-27 19:27:20 +0300

    Add project files.

---
+++ Task3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Используя Visual Studio, создайте проект по шаблону Console Application.  
+Создайте анонимный метод, который принимает в качестве аргумента массив делегатов и возвращает среднее арифметическое возвращаемых значений методов сообщенных с делегатами в массиве. 
+Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int. 
+*/
+using System;
+
+namespace Task3
+{
+    class Program
+    {
+        delegate int MyDelegateMas(MyDelegate[] myDel);
+        delegate int MyDelegate();
+
+        static void Main(string[] args)
+        {
+            MyDelegate[] myDel;
+            MyDelegateMas myDelMas;
+            int amountOfElement;
+
+            Console.Write("Количество элементов: ");
+            amountOfElement = Int32.Parse(Console.ReadLine());
+
+            myDel = new MyDelegate[amountOfElement];
+            int result = 0;
+
+            Random random = new Random();
+
+            for (int i = 0; i < myDel.Length; i++)
+            {
+                myDel[i] = () => random.Next(200);
+            }
+
+            myDelMas = myDel =>
+            {
+                for (int i = 0; i < myDel.Length; i++)
+                {
+                    result = myDel[i]();
+                }
+                return result / myDel.Length;
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
